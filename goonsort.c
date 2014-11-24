53c85ce1ae8de0eee60db0b7460f65ecc654129a
mhyfritz <markus.hsi.yang@gmail.com>
2014-11-24 13:00:20 +0100

    Add command line handling

--- goonsort.c
+++ goonsort.c
@@ -1,13 +1,53 @@
 #include "goonsort.h"
 
+static void usage()
+{
+    fprintf(stderr, "\n");
+    fprintf(stderr, "GOON sort\n");
+    fprintf(stderr, "\n");
+    fprintf(stderr, "arguments:\n");
+    fprintf(stderr, "    -s/--seqkey     sequence key (mandatory)\n");
+    fprintf(stderr, "    -b/--startkey   start position key (mandatory)\n");
+    fprintf(stderr, "    -h/--help       display help\n");
+    fprintf(stderr, "\n");
+}
+
 int goonsort(int argc, char *argv[])
 {
-    int i;
+    struct option opts[] = {
+        {"seqkey", required_argument, NULL, 's'},
+        {"startkey", required_argument, NULL, 'b'},
+        {"help", no_argument, NULL, 'h'},
+        {NULL, 0, NULL, 0}
+    };
+    int c;
+    char *seq_key = NULL, *start_key = NULL;
 
-    printf("GOON sort\n");
-    for (i = 0; i < argc; i += 1) {
-        printf("  %s\n", argv[i]);
+    if (argc == 1) {
+        usage();
+        return -1;
+    }
+
+    while ((c = getopt_long(argc,
+                            argv,
+                            "s:b:h",
+                            opts,
+                            NULL)) != -1) {
+        switch (c) {
+            case 'h': usage();
+                      return -1;
+            case 's': seq_key = optarg;
+                      break;
+            case 'b': start_key = optarg;
+                      break;
+            default: return -1;
+        }
+    }
+
+    if (seq_key == NULL || start_key == NULL) {
+        usage();
+        return -1;
     }
 
     return 0;
-}
+} 
--- goonsort.h
+++ goonsort.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <getopt.h>
 
 #include "goonsort.p"
 
