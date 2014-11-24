bd32f55d16fba027573508a78cf4a7f9ec256e28
Markus <mhyf@oni.localdomain>
2014-11-24 22:48:22 +0100

    Start goonindex command line handling

--- goonindex.h
+++ goonindex.h
@@ -3,7 +3,17 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <getopt.h>
+#include <sys/stat.h>
+
+#include "bgzf.h"
 
 #include "goonindex.p"
+
+typedef struct {
+    char *seq_key,
+         *start_key,
+         *end_key;
+} Gn_index_conf;
 
 #endif
