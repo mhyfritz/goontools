4143830e254e0bb364ad665149d772a661d91bf6
mhyfritz <markus.hsi.yang@gmail.com>
2014-11-25 16:27:07 +0100

    Start CLI

--- src/goonview.c
+++ src/goonview.c
@@ -1,12 +1,23 @@
 #include "goonview.h"
 
+#define PROG_ALIAS "goonview"
+#define USAGE usage(PROG_ALIAS)
+
+static void usage(char *prog)
+{
+    fprintf(stderr, "\n");
+    fprintf(stderr, "Usage: %s <arguments> <region1> [region2 [...]]\n", prog);
+    fprintf(stderr, "\n");
+    fprintf(stderr, "arguments:\n");
+    fprintf(stderr, "    -f/--file     input goon.bgz file (mandatory)\n");
+    fprintf(stderr, "\n");
+}
+
 int goonview(int argc, char *argv[])
 {
-    int i;
-
-    printf("GOON view\n");
-    for (i = 0; i < argc; i += 1) {
-        printf("  %s\n", argv[i]);
+    if (argc == 1) {
+        USAGE;
+        return -1;
     }
 
     return 0;
