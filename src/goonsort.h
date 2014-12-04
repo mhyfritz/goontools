4f085bbeeb28d7b57db2c6febb03dda8374af214
mhyfritz <markus.hsi.yang@gmail.com>
2014-12-04 16:22:12 +0100

    Start threaded/on-disk sorting

--- src/goonsort.h
+++ src/goonsort.h
@@ -7,12 +7,16 @@
 #include <stdint.h>
 #include <sys/stat.h>
 #include <assert.h>
+#include <pthread.h>
 
 #include "array.h"
 #include "ksort.h"
 #include "goon.h"
+#include "bgzf.h"
 
 #include "goonsort.p"
+
+#define MAX_MEM_MB_DEFAULT (size_t)756
 
 typedef struct {
     int32_t pos;
@@ -20,5 +24,12 @@ typedef struct {
 } Gn_sort_record_t;
 
 typedef Gn_sort_record_t *Gn_sort_record_p;
+
+typedef struct {
+    char *seq_key,
+         *start_key;
+    size_t max_mem;
+    int n_threads;
+} Gn_sort_conf;
 
 #endif
