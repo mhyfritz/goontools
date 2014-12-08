f304c2c61b9326e2cd2246ac67a62a0d1cc0a446
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2014-12-08 21:18:26 +0100

    Make file argument click.File

--- util/ldjconv.py
+++ util/ldjconv.py
@@ -8,10 +8,6 @@ import gzip
 from collections import OrderedDict
 
 
-def opener(fn):
-    return gzip.open if fn.endswith('.gz') else open
-
-
 @click.group()
 def cli():
     pass
@@ -24,42 +20,40 @@ def cli():
               help='column names comma-separated')
 @click.option('na', '-n', '--na', default=None,
               help='NA value to omit')
-@click.argument('fn', metavar='FILE')
-def toldj(sep, colnames, na, fn):
+@click.argument('infile', type=click.File())
+def toldj(sep, colnames, na, infile):
     sep = sep.decode('string-escape')
-    with opener(fn)(fn) as f:
-        f_reader = csv.reader(f, delimiter=sep)
-        if colnames is not None:
-            colnames = colnames.split(',')
-        else:
-            colnames = next(f_reader)
-        for fields in f_reader:
-            d = {c: f for c, f in zip(colnames, fields) if f != na}
-            print(json.dumps(d))
+    f_reader = csv.reader(infile, delimiter=sep)
+    if colnames is not None:
+        colnames = colnames.split(',')
+    else:
+        colnames = next(f_reader)
+    for fields in f_reader:
+        d = {c: f for c, f in zip(colnames, fields) if f != na}
+        print(json.dumps(d))
 
 
 @click.command()
 @click.option('sep', '-d', '--delimiter', default='\t',
               help='delimiter')
-@click.option('colnames', '-c', '--colnames',
+@click.option('colnames', '-c', '--colnames', metavar='CSV',
               help='column names comma-separated')
 @click.option('na', '-n', '--na', default='',
               help='NA value to insert')
-@click.argument('fn', metavar='FILE')
-def fromldj(sep, colnames, na, fn):
+@click.argument('infile', type=click.File())
+def fromldj(sep, colnames, na, infile):
     sep = sep.decode('string-escape')
-    with opener(fn)(fn) as f:
-        if colnames is not None:
-            colnames = colnames.split(',')
-        else:
-            line = next(f)
-            d = json.JSONDecoder(object_pairs_hook=OrderedDict).decode(line)
-            colnames = d.keys()
-            f.seek(0)
-        print(sep.join(colnames))
-        for line in f:
-            d = json.loads(line)
-            print(sep.join(str(d.get(c, na)) for c in colnames))
+    if colnames is not None:
+        colnames = colnames.split(',')
+    else:
+        line = next(infile)
+        d = json.JSONDecoder(object_pairs_hook=OrderedDict).decode(line)
+        colnames = d.keys()
+        infile.seek(0)
+    print(sep.join(colnames))
+    for line in infile:
+        d = json.loads(line)
+        print(sep.join(str(d.get(c, na)) for c in colnames))
 
 cli.add_command(toldj)
 cli.add_command(fromldj)
