82855e7b31b55d7fb74f27ed1348d1295a8540e0
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2014-12-31 16:35:02 +0100

    First commit of

---
+++ src/goonextract.c
@@ -0,0 +1,95 @@
+#include "goonextract.h"
+
+#define PROG_ALIAS "goonextract"
+#define USAGE usage(PROG_ALIAS)
+
+static void usage(char *prog)
+{
+    fprintf(stderr, "\n");
+    fprintf(stderr, "Usage: %s [arguments] <goonfile> "
+                    "<key1> [key2 ... ]]\n", prog);
+    fprintf(stderr, "\n");
+    fprintf(stderr, "arguments:\n");
+    fprintf(stderr, "    -d/--delim  field delimiter character\n");
+    fprintf(stderr, "    -h/--help   display help\n");
+    fprintf(stderr, "\n");
+}
+
+int goonextract(int argc, char *argv[])
+{
+    struct option opts[] = {
+        {"delim", required_argument, NULL, 'd'},
+        {"help", no_argument, NULL, 'h'},
+        {NULL, 0, NULL, 0}
+    };
+    int c,
+        i;
+    char *d = DEFAULT_DELIMITER,
+         line[65536]; // FIXME
+    FILE *fp;
+    kson_t *kson = 0;
+    const kson_node_t *p;
+    
+    if (argc < 2) {
+        USAGE;
+        return -1;
+    }
+
+    while ((c = getopt_long(argc,
+                            argv,
+                            "d:h",
+                            opts,
+                            NULL)) != -1) {
+        switch (c) {
+            case 'h': USAGE;
+                      return -1;
+            case 'd': d = optarg;
+                      break;
+            default: return -1;
+        }        
+    }
+
+    if (optind >= argc -1) {
+        fprintf(stderr, "error: missing arguments\n");
+        return -1;
+    }
+
+    if (strcmp(argv[optind], "-") == 0) {
+        fp = stdin;
+    } else {
+        if ((fp = fopen(argv[optind], "r")) == NULL) {
+            fprintf(stderr, "error: cannot open file %s\n", argv[optind]);
+            return -1;
+        }
+    }
+
+    while(fgets(line, 65536, fp) != NULL) {
+        kson = kson_parse(line);
+        if (kson) {
+            for (i = optind + 1; i < argc; i += 1) {
+                p = kson_by_key(kson->root, argv[i]);
+                if (!p) {
+                    fprintf(stderr, "error: cannot find key %s\n", argv[i]);
+                    return -1;
+                }
+                if (kson_is_internal(p)) {
+                    fprintf(stderr, "error: compound value %s\n", argv[i]);
+                    return -1;
+                }
+                if (i > optind + 1) {
+                    printf("%s", d);
+                }
+                printf("%s", p->v.str);
+            }
+            printf("\n");
+        } else {
+            fprintf(stderr, "cannot parse line: %s", line);
+            return -1;
+        }
+    }
+
+    fclose(fp);
+    kson_destroy(kson);
+
+    return 0;
+}
---
+++ src/goonextract.h
@@ -0,0 +1,15 @@
+#ifndef GOONEXTRACT_H
+#define GOONEXTRACT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <getopt.h>
+
+#include "klib/kson.h"
+#include "bgzf.h"
+
+#include "goonextract.p"
+
+#define DEFAULT_DELIMITER "\t"
+
+#endif
