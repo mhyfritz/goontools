f16cec550893797d044b6e6280a5d227f41c7c52
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2015-01-01 14:05:07 +0100

    Allow arbitrary paths

--- src/goonextract.c
+++ src/goonextract.c
@@ -6,13 +6,13 @@
 static void usage(char *prog)
 {
     fprintf(stderr, "\n");
-    fprintf(stderr, "Usage: %s [arguments] <goonfile> "
-                    "<key1> [key2 ... ]\n", prog);
+    fprintf(stderr, "Usage: %s [arguments] [goonfile]\n", prog);
     fprintf(stderr, "\n");
     fprintf(stderr, "arguments:\n");
+    fprintf(stderr, "    -p/--path    extraction path\n"
+                    "                 (can be passed multiple time)\n");
     fprintf(stderr, "    -d/--delim   field delimiter\n");
     fprintf(stderr, "    -n/--null    null value\n");
-    fprintf(stderr, "    -e/--header  print header\n");
     fprintf(stderr, "    -h/--help    display help\n");
     fprintf(stderr, "\n");
 }
@@ -20,6 +20,7 @@ static void usage(char *prog)
 int goonextract(int argc, char *argv[])
 {
     struct option opts[] = {
+        {"path", required_argument, NULL, 'p'},
         {"delim", required_argument, NULL, 'd'},
         {"null", required_argument, NULL, 'n'},
         {"help", no_argument, NULL, 'h'},
@@ -27,79 +28,91 @@ int goonextract(int argc, char *argv[])
     };
     int c,
         i,
-        header = 0;
+        j;
     char *d = DEFAULT_DELIMITER,
          *null = NULL,
          line[65536]; // FIXME
     FILE *fp;
     kson_t *kson = 0;
     const kson_node_t *p;
+    ArrayPath paths;
+    Path path;
     
-    if (argc < 3) {
+    ARRAY_INIT(&paths, Path, 64);
+
+    if (argc == 1) {
         USAGE;
         return -1;
     }
 
     while ((c = getopt_long(argc,
                             argv,
-                            "d:n:eh",
+                            "p:d:n:e",
                             opts,
                             NULL)) != -1) {
         switch (c) {
             case 'h': USAGE;
                       return -1;
+            case 'p': path.start = optind - 1;
+                      for (i = path.start;
+                           i < argc - 1 && argv[i][0] != '-';
+                           i += 1) {
+                        // empty
+                      }
+                      path.end = i - 1;
+                      ARRAY_PUSH(&paths, Path, path);
+                      optind = i;
+                      break;
             case 'd': d = optarg;
                       break;
             case 'n': null = optarg;
-                      break;
-            case 'e': header = 1;
                       break;
             default: return -1;
         }        
     }
 
-    if (optind >= argc -1) {
-        fprintf(stderr, "error: missing arguments\n");
+    if (paths.nextfree == 0) {
+        fprintf(stderr, "error: missing path(s)\n");
         return -1;
+
     }
 
-    if (strcmp(argv[optind], "-") == 0) {
+    if (strcmp(argv[argc-1], "-") == 0) {
         fp = stdin;
     } else {
-        if ((fp = fopen(argv[optind], "r")) == NULL) {
-            fprintf(stderr, "error: cannot open file %s\n", argv[optind]);
+        if ((fp = fopen(argv[argc-1], "r")) == NULL) {
+            fprintf(stderr, "error: cannot open file %s\n", argv[argc-1]);
             return -1;
         }
-    }
-
-    if (header) {
-        for (i = optind + 1; i < argc; i += 1) {
-            if (i > optind + 1) {
-                printf("%s", d);
-            }
-            printf("%s", argv[i]);
-        }
-        printf("\n");
     }
 
     while(fgets(line, 65536, fp) != NULL) {
         kson = kson_parse(line);
         if (kson) {
-            for (i = optind + 1; i < argc; i += 1) {
-                p = kson_by_key(kson->root, argv[i]);
+            for (i = 0; i < paths.nextfree; i += 1) {
+                p = kson->root;
+                path = paths.elems[i];
+                for (j = path.start; j <= path.end; j += 1) {
+                    if (p->type == KSON_TYPE_BRACKET) {
+                        p = kson_by_index(p, atoi(argv[j]));
+                    } else if (p->type == KSON_TYPE_BRACE) {
+                        p = kson_by_key(p, argv[j]);
+                    } else {
+                        p = NULL;
+                    }
+                }
                 if (!p && !null) {
-                    fprintf(stderr, "error: cannot find key %s\n", argv[i]);
+                    fprintf(stderr, "error: cannot find '%s'\n",
+                            argv[j]);
                     return -1;
                 }
                 if (p && kson_is_internal(p)) {
-                    fprintf(stderr, "error: compound value %s\n", argv[i]);
+                    fprintf(stderr, "error: compound value '%s'\n",
+                            argv[j]);
                     return -1;
                 }
-                if (i > optind + 1) {
-                    printf("%s", d);
+                printf("%s%s", i > 0 ? d : "", p ? p->v.str : null);
                 }
-                printf("%s", p ? p->v.str : null);
-            }
             printf("\n");
         } else {
             fprintf(stderr, "cannot parse line: %s", line);
@@ -109,6 +122,7 @@ int goonextract(int argc, char *argv[])
 
     fclose(fp);
     kson_destroy(kson);
+    ARRAY_FREE(&paths);
 
     return 0;
 }
--- src/goonextract.h
+++ src/goonextract.h
@@ -7,9 +7,17 @@
 
 #include "klib/kson.h"
 #include "bgzf.h"
+#include "array.h"
 
 #include "goonextract.p"
 
 #define DEFAULT_DELIMITER "\t"
+
+typedef struct {
+    size_t start,
+           end;
+} Path;
+
+ARRAY_DECLARE(Path);
 
 #endif
